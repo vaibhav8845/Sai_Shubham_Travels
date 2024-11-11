c315271d9255f47e3048de68dda3191f6e3c58ec
vaibhav8845 <vaibhavsonawane2005@gmail.com>
2024-11-11 17:58:21 +0530

    Your commit message

--- src/components/TaxiForm/TaxiForm.js
+++ src/components/TaxiForm/TaxiForm.js
@@ -336,6 +336,8 @@ function TaxiForm() {
                 <option value="Sedan">Sedan</option>
                 <option value="SUV">SUV</option>
                 <option value="Bus">Bus</option>
+                <option value="MiniBus">MiniBus</option>
+
                 <option value="Minivan">Minivan</option>
               </select>
               {errors.vehicleType && <div className="invalid-feedback">{errors.vehicleType}</div>}
